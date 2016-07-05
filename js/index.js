d4d5c085c95e0dd950b3ffc4927d7830996e59f3
Jonah <jonah.stieglitz17@gmail.com>
2016-07-05 12:48:46 -0500

    Base Layout Implemented
    Copy for use on each page

--- js/index.js
+++ js/index.js
@@ -2,3 +2,17 @@ var reverse = function(toReverse)
 {
   console.log(toReverse.split("").reverse().join(""));
 }
+
+/*
+ * TODO : Message Board
+ * TODO : Project's Pages
+ *         - Home Page Shows All Creations (As You Scroll Down More Show Up - Instagram)
+ *         - Categories (Java, Java GUI's, Software, Libraries, C#, C# GUI's, etc.)
+ *         - Search Functionality
+ *         - When You Click One You Go To A Page Containing:
+ *             - Github Info
+ *             - Screen Shots
+ *             - User Manual / Tutorial For Libs/etc.
+ *
+ *
+ */
